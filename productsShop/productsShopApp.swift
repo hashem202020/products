9f1d2a327579fc577865387ce5aeb9579842f19e
hashem202020 <75537854+hashem202020@users.noreply.github.com>
2024-12-07 16:11:07 +0200

    Initial Commit

---
+++ productsShop/productsShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  productsShopApp.swift
+//  productsShop
+//
+//  Created by Khaled hashem on 07/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct productsShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
